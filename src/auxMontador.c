3996c989ee3d9b40a54da0614a6f07ad6b928143
Julio Moreira B. Barros <julio.barros@playkids.com>
2018-09-12 18:04:27 -0300

    funçoes aux porontas

--- src/auxMontador.c
+++ src/auxMontador.c
@@ -3,13 +3,42 @@
 //
 
 #include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char * paraMaiuscula(const char *palavra) {
+    size_t len = strlen(palavra);
+    char *maiuscula = (char *) malloc(len* sizeof(char));
+    char decremento = 'a' - 'A';
+
+    for (int i = 0; i < len; ++i) {
+        if (palavra[0] >= 65 && palavra[0] <= 90) {
+            maiuscula[i] = palavra[i] - decremento;
+        } else {
+            maiuscula[i] = palavra[i];
+        }
+    }
+
+    return maiuscula;
+}
 
 int eHexadecimal(char* palavra) {
-    if (strlen(palavra) <= 2) {
+    size_t len = strlen(palavra);
+    if (len <= 2 || len > 12) {
         return 0;
     }
 
     if (palavra[0] == '0' && palavra[1] == 'x') {
+
+        long numeroParseado;
+        char* ptrErro;
+
+        numeroParseado = strtol(palavra, &ptrErro, 16);
+
+        if (ptrErro != (palavra + strlen(palavra))) {
+            return 0;
+        }
+
         return 1;
     }
 
@@ -17,9 +46,55 @@ int eHexadecimal(char* palavra) {
 }
 
 int eDiretiva(char* palavra) {
+    size_t len = strlen(palavra);
+    if (len < 2) {
+        // diretiva mt curta
+        return 0;
+    }
 
+    if (palavra[0] != '.') {
+        // sem ponto no inicio
+        return 0;
+    }
+
+    char *maiuscula = paraMaiuscula(palavra);
+
+    if (strcmp(maiuscula, ".SET") == 0) { return 1; }
+    if (strcmp(maiuscula, ".ORG") == 0) { return 1; }
+    if (strcmp(maiuscula, ".ALIGN") == 0) { return 1; }
+    if (strcmp(maiuscula, ".WFILL") == 0) { return 1; }
+    if (strcmp(maiuscula, ".WORD") == 0) { return 1; }
+
+    // desconhecida
+    return 0;
 }
 
-int eRotulo(char* palavre) {
+int eRotulo(char* palavra) {
+    size_t len = strlen(palavra);
+    if (len < 2) {
+        // rotulo mt curto
+        return 0;
+    }
 
+    if (palavra[len - 1] != ':') {
+        // rotulo nao terminado em :
+        return 0;
+    }
+
+    if (palavra[0] >= 48 && palavra[0] <= 57) {
+        // numero no inicio
+        return 0;
+    }
+
+    for (int i = 0; i < len - 1; ++i) {
+
+        if (palavra[0] >= 48 && palavra[0] <= 57) { continue; }
+        if (palavra[0] >= 65 && palavra[0] <= 90) { continue; }
+        if (palavra[0] >= 97 && palavra[0] <= 122) { continue; }
+
+        // caracter invalido
+        return 0;
+    }
+
+    return 1;
 }
